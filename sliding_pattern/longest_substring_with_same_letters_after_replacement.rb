ec20743c7300b80af502d7864a73eaefa95321a3
see-why <iyadicy@gmail.com>
2023-07-06 00:22:56 +0100

    Longest Substring with Same Letters after Replacement (hard)

---
+++ sliding_pattern/longest_substring_with_same_letters_after_replacement.rb
@@ -0,0 +1,23 @@
+# String="aabccbb", k=2
+
+def longest_substring_with_same_letters_after_replacement str, k
+  window = {}
+  window_start = longest_substring_length = maximum_char_count = 0
+  str.each_char.with_index do |char, window_end|
+    if window_end - maximum_char_count > k
+      start_char = str[window_start]
+      window[start_char] -= 1
+      window_start += 1
+    end
+
+    window[char] = 0 unless window.key? char
+    window[char] += 1
+    maximum_char_count = [maximum_char_count, window[char]].max
+    longest_substring_length = [longest_substring_length, (maximum_char_count + k)].max
+  end
+  longest_substring_length
+end
+
+pp longest_substring_with_same_letters_after_replacement 'aabccbb', 2
+pp longest_substring_with_same_letters_after_replacement 'abbcb', 1
+pp longest_substring_with_same_letters_after_replacement 'abccde', 1
