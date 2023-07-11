b4f45de0a46de839c16ac245a5cc66db3b2058cf
see-why <iyadicy@gmail.com>
2023-07-11 02:02:22 +0100

    refactor use hash map over sorting

--- sliding_pattern/problem_challenge_1.rb
+++ sliding_pattern/problem_challenge_1.rb
@@ -1,5 +1,5 @@
-# String="oidbcaf", Pattern="abc"
-
+# sorting the window to compare to the pattern...sorting can be expensive
+# in large strings
 def permutation_in_a_string(str, pattern)
   sorted_pattern = pattern.chars.sort.join
   window = ''
@@ -15,7 +15,43 @@ def permutation_in_a_string(str, pattern)
   false
 end
 
+# using a hash map
+def permutation_in_a_string_with_map(str, pattern)
+  window = ''
+  char_map = {}
+  matched = 0
+
+  pattern.each_char do |char|
+    char_map[char] = 0 unless char_map.key? char
+    char_map[char] += 1
+  end
+
+  str.each_char do |char|
+    window += char
+    if char_map.key? char
+      char_map[char] -= 1 if char_map[char].positive?
+      matched += 1 if char_map[char].zero?
+    end
+    next unless window.size == pattern.size
+
+    return true if matched == char_map.keys.size
+
+    first_char = window.chars[0]
+    if char_map.key? first_char
+      matched -= 1
+      char_map[first_char] += 1
+    end
+    window = window.chars[1..-1].join
+  end
+  false
+end
+
 pp permutation_in_a_string 'oidbcaf', 'abc'
 pp permutation_in_a_string 'odicf', 'dc'
 pp permutation_in_a_string 'bcdxabcdy', 'bcdyabcdx'
 pp permutation_in_a_string 'aaacb', 'abc'
+
+pp permutation_in_a_string_with_map 'oidbcaf', 'abc'
+pp permutation_in_a_string_with_map 'odicf', 'dc'
+pp permutation_in_a_string_with_map 'bcdxabcdy', 'bcdyabcdx'
+pp permutation_in_a_string_with_map 'aaacb', 'abc'
