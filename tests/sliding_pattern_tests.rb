a7fb5574cb601877e043e26a4f86a2d354ecde7e
see-why <iyadicy@gmail.com>
2023-07-19 23:55:14 +0100

    set up tests

---
+++ tests/sliding_pattern_tests.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'average_of_subarray_of_size_k'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestSlidingPattern < Test::Unit::TestCase
+  def test_average_of_subarray_of_size_k
+    assert_equal([2.2, 2.8, 2.4, 3.6, 2.8], average_of_subarray_of_size_k([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
+  end
+end
