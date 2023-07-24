e1686fc9421a045b01cca679c14092e613ae7256
see-why <iyadicy@gmail.com>
2023-07-24 23:12:46 +0100

    remove duplicates in array solution and test

---
+++ pattern_two_pointers/remove_duplicates.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# expects a sorted arr
+def remove_duplicates(arr)
+  i = next_non_dup = 1
+
+  while i < arr.size
+    if arr[next_non_dup - 1] != arr[i]
+      arr[next_non_dup] = arr[i]
+      next_non_dup += 1
+    end
+    i += 1
+  end
+  next_non_dup
+end
+
+def remove_duplicates_of_target(arr, target)
+  i = next_non_dup = 0
+
+  while i < arr.size
+    if arr[i] != target
+      arr[next_non_dup] = arr[i]
+      next_non_dup += 1
+    end
+    i += 1
+  end
+  next_non_dup
+end
---
+++ tests/pattern_two_pointers_tests/remove_duplicates_tests.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../../pattern_two_pointers/remove_duplicates'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestSlidingPattern < Test::Unit::TestCase
+  def test_remove_duplicates
+    assert_equal(4, remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+    assert_equal(2, remove_duplicates([2, 2, 2, 11]))
+  end
+
+  def test_remove_duplicates_of_target
+    assert_equal(4, remove_duplicates_of_target([3, 2, 3, 6, 3, 10, 9, 3], 3))
+    assert_equal(2, remove_duplicates_of_target([2, 11, 2, 2, 1], 2))
+  end
+end
