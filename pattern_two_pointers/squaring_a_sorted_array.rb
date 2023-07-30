4e54c0eac182c53f5665d384b08e32b840283c57
see-why <iyadicy@gmail.com>
2023-07-30 21:00:16 +0100

    brute force solution

--- pattern_two_pointers/squaring_a_sorted_array.rb
+++ pattern_two_pointers/squaring_a_sorted_array.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+def squaring_a_sorted_array(arr)
+  result = [arr.first**2]
+
+  (1..arr.size - 1).each do |ind|
+    temp = arr[ind]**2
+    (0..result.size - 1).each do |i|
+      if result[i] >= temp
+        result.insert(i, temp)
+        break
+      end
+    end
+    result.push temp if temp > result.last
+  end
+
+  result
+end
