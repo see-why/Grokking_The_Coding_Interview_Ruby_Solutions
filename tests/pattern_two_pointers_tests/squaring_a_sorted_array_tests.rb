8bae9210be47a1f685966980248b825c24cb734c
see-why <iyadicy@gmail.com>
2023-07-30 21:00:23 +0100

    test files

--- tests/pattern_two_pointers_tests/squaring_a_sorted_array_tests.rb
+++ tests/pattern_two_pointers_tests/squaring_a_sorted_array_tests.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+require_relative '../../pattern_two_pointers/squaring_a_sorted_array'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestPatternTwoPointers < Test::Unit::TestCase
+  def test_squaring_a_sorted_array
+    assert_equal([0, 1, 4, 4, 9], squaring_a_sorted_array([-2, -1, 0, 2, 3]))
+    assert_equal([0, 1, 1, 4, 9], squaring_a_sorted_array([-3, -1, 0, 1, 2]))
+  end
+end
