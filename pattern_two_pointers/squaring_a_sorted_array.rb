aa52fd2d34001ab674aa9cdb7e2f3a3985f98321
see-why <iyadicy@gmail.com>
2023-07-30 21:21:13 +0100

    optimised two pointer method

--- pattern_two_pointers/squaring_a_sorted_array.rb
+++ pattern_two_pointers/squaring_a_sorted_array.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-def squaring_a_sorted_array(arr)
+# brute force solution that expects a sorted array
+def squaring_a_sorted_array_brute_force(arr)
   result = [arr.first**2]
 
   (1..arr.size - 1).each do |ind|
@@ -14,5 +15,28 @@ def squaring_a_sorted_array(arr)
     result.push temp if temp > result.last
   end
 
+  result
+end
+
+def squaring_a_sorted_array(arr)
+  result = Array.new(arr.size).fill(0)
+  left = 0
+  right = arr.size - 1
+  i = 1
+
+  while left <= right
+    left_square = arr[left]**2
+    right_square = arr[right]**2
+
+    if left_square > right_square
+      result[(arr.size - i)] = left_square
+      left += 1
+    else
+      result[(arr.size - i)] = right_square
+      right -= 1
+    end
+
+    i += 1
+  end
   result
 end
--- tests/pattern_two_pointers_tests/squaring_a_sorted_array_tests.rb
+++ tests/pattern_two_pointers_tests/squaring_a_sorted_array_tests.rb
@@ -5,6 +5,11 @@ require 'test/unit'
 
 # test class for the sliding pattern solutions
 class TestPatternTwoPointers < Test::Unit::TestCase
+  def test_squaring_a_sorted_array_brute_force
+    assert_equal([0, 1, 4, 4, 9], squaring_a_sorted_array_brute_force([-2, -1, 0, 2, 3]))
+    assert_equal([0, 1, 1, 4, 9], squaring_a_sorted_array_brute_force([-3, -1, 0, 1, 2]))
+  end
+
   def test_squaring_a_sorted_array
     assert_equal([0, 1, 4, 4, 9], squaring_a_sorted_array([-2, -1, 0, 2, 3]))
     assert_equal([0, 1, 1, 4, 9], squaring_a_sorted_array([-3, -1, 0, 1, 2]))
