7bcb789b51600b90ac8de6e497e6f05dd1ceadca
see-why <iyadicy@gmail.com>
2023-08-03 00:39:15 +0100

    return triplets in an array

--- pattern_two_pointers/triplets_with_smaller_sum.rb
+++ pattern_two_pointers/triplets_with_smaller_sum.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# returns count
 def triplets_with_smaller_sum(arr, target)
   arr = arr.sort
   triplets_count = 0
@@ -18,4 +19,27 @@ def triplets_with_smaller_sum(arr, target)
     end
   end
   triplets_count
+end
+
+# returns triplets in array
+def triplets_with_smaller_sum_array(arr, target)
+  arr = arr.sort
+  triplets = []
+  (arr.size - 2).times do |i|
+    left = i + 1
+    right = arr.size - 1
+
+    while left < right
+      current_sum = arr[i] + arr[left] + arr[right]
+      if current_sum < target
+        (right - left).times do |j|
+          triplets << [arr[i], arr[left], arr[right - j]]
+        end
+        left += 1
+      else
+        right -= 1
+      end
+    end
+  end
+  triplets
 end
--- tests/pattern_two_pointers_tests/triplets_with_smaller_sum_tests.rb
+++ tests/pattern_two_pointers_tests/triplets_with_smaller_sum_tests.rb
@@ -9,4 +9,10 @@ class TestPatternTwoPointers < Test::Unit::TestCase
     assert_equal(2, triplets_with_smaller_sum([-1, 0, 2, 3], 3))
     assert_equal(4, triplets_with_smaller_sum([-1, 4, 2, 1, 3], 5))
   end
+
+  def test_triplets_with_smaller_sum_array
+    assert_equal([[-1, 0, 3], [-1, 0, 2]], triplets_with_smaller_sum_array([-1, 0, 2, 3], 3))
+    assert_equal([[-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]],
+                 triplets_with_smaller_sum_array([-1, 4, 2, 1, 3], 5))
+  end
 end
