4b700268283b8a3f4cd381fea26c477ebcec7032
see-why <iyadicy@gmail.com>
2023-08-03 23:15:30 +0100

    rename method

--- tests/pattern_two_pointers_tests/subarrays_with_product_less_than_target_tests.rb
+++ tests/pattern_two_pointers_tests/subarrays_with_product_less_than_target_tests.rb
@@ -5,7 +5,7 @@ require 'test/unit'
 
 # test class for the sliding pattern solutions
 class TestPatternTwoPointers < Test::Unit::TestCase
-  def test_triplet_sum_close_to_target
+  def test_subarrays_with_product_less_than_target
     second_answer = [[8], [2], [8, 2], [6], [2, 6], [5], [6, 5]]
 
     assert_equal([[2], [5], [2, 5], [3], [5, 3], [10]], subarrays_with_product_less_than_target([2, 5, 3, 10], 30))
