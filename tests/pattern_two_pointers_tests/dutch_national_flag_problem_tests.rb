69211248250f9d351e9438f1965c1f85b57afae2
see-why <iyadicy@gmail.com>
2023-08-09 23:23:49 +0100

    add more tests

--- tests/pattern_two_pointers_tests/dutch_national_flag_problem_tests.rb
+++ tests/pattern_two_pointers_tests/dutch_national_flag_problem_tests.rb
@@ -8,5 +8,6 @@ class TestPatternTwoPointers < Test::Unit::TestCase
   def test_dutch_national_flag_problem
     assert_equal([0, 0, 1, 1, 2], dutch_national_flag_problem([1, 0, 2, 1, 0]))
     assert_equal([0, 0, 1, 2, 2, 2], dutch_national_flag_problem([2, 2, 0, 1, 2, 0]))
+    assert_equal([0, 0, 0, 0, 1, 1, 2, 2, 2], dutch_national_flag_problem([0, 2, 0, 2, 0, 1, 2, 0, 1]))
   end
 end
