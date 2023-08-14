f6bd64971a4da9cc2936d578b58ae967bb3231b9
see-why <iyadicy@gmail.com>
2023-08-14 22:05:13 +0100

    problem_challenge_2 test files

---
+++ tests/pattern_two_pointers_tests/problem_challenge_2_tests.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../../pattern_two_pointers/problem_challenge_2'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestPatternTwoPointers < Test::Unit::TestCase
+  def test_comparing_strings_containing_backspaces
+    assert_equal(true, comparing_strings_containing_backspaces('xy#z', 'xzz#'))
+    assert_equal(false, comparing_strings_containing_backspaces('xy#z', 'xyz#'))
+    assert_equal(true, comparing_strings_containing_backspaces('xp#', 'xyz##'))
+    assert_equal(true, comparing_strings_containing_backspaces('xywrrmp', 'xywrrmu#p'))
+  end
+end
