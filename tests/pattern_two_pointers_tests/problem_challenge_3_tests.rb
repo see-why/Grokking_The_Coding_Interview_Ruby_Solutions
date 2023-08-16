5ba37381f69d28c20a357a38a2488018800ccb6d
see-why <iyadicy@gmail.com>
2023-08-16 23:08:32 +0100

    test files

---
+++ tests/pattern_two_pointers_tests/problem_challenge_3_tests.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../../pattern_two_pointers/problem_challenge_3'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestPatternTwoPointers < Test::Unit::TestCase
+  def test_minimum_window_sort
+    assert_equal(5, minimum_window_sort([1, 3, 2, 0, -1, 7, 10]))
+    assert_equal(5, minimum_window_sort([1, 2, 5, 3, 7, 10, 9, 12]))
+    assert_equal(0, minimum_window_sort([1, 2, 3]))
+    assert_equal(3, minimum_window_sort([3, 2, 1]))
+  end
+end
