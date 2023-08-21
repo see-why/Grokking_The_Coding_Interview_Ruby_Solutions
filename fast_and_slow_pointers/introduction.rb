c7e99b8d4d2cbcbebed866d07617b2da8d82d2a0
see-why <iyadicy@gmail.com>
2023-08-21 02:58:13 +0100

    find the length of the linked list cycle

--- fast_and_slow_pointers/introduction.rb
+++ fast_and_slow_pointers/introduction.rb
@@ -11,3 +11,26 @@ def linked_list_cycle(head)
   end
   false
 end
+
+def find_cycle_length(head)
+  slow = fast = head
+
+  until fast.nil?
+    slow = slow.next
+    fast = fast.next.next
+
+    return calculate_cycle_length(slow) if slow == fast
+  end
+end
+
+def calculate_cycle_length(slow)
+  counter = 0
+  current_slow = slow
+  loop do
+    slow = slow.next
+    counter += 1
+    break if slow == current_slow
+  end
+
+  counter
+end
