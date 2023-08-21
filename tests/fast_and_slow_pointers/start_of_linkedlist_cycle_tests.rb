27e26e6c64c61174998a6a6760c3560bf10b6f3b
see-why <iyadicy@gmail.com>
2023-08-21 03:24:53 +0100

    start_of_linkedlist_cycle test files

---
+++ tests/fast_and_slow_pointers/start_of_linkedlist_cycle_tests.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative '../../fast_and_slow_pointers/start_of_linkedlist_cycle'
+require_relative '../helpers/node_helper'
+require 'test/unit'
+
+# test class for the sliding pattern solutions
+class TestFastAndSlowPointers < Test::Unit::TestCase
+  include NodeHelper
+
+  def test_start_of_linkedlist_cycle
+    head = list_head
+
+    head.next.next.next.next.next.next = head.next.next
+    assert_equal(3, start_of_linkedlist_cycle(head))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    assert_equal(4, start_of_linkedlist_cycle(head))
+
+    head.next.next.next.next.next.next = head
+    assert_equal(1, start_of_linkedlist_cycle(head))
+  end
+end
