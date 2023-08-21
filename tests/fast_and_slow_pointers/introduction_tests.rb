2aedd1147c141f895c9062571190bee50c40254e
see-why <iyadicy@gmail.com>
2023-08-21 03:23:37 +0100

    refactor to use node helper module

--- tests/fast_and_slow_pointers/introduction_tests.rb
+++ tests/fast_and_slow_pointers/introduction_tests.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 require_relative '../../fast_and_slow_pointers/introduction'
-require_relative '../../lib/node'
+require_relative '../helpers/node_helper'
 require 'test/unit'
 
 # test class for the sliding pattern solutions
 class TestFastAndSlowPointers < Test::Unit::TestCase
-  def list_head
-    head = Node.new(1)
-    head.next = Node.new(2)
-    head.next.next = Node.new(3)
-    head.next.next.next = Node.new(4)
-    head.next.next.next.next = Node.new(5)
-    head.next.next.next.next.next = Node.new(6)
-
-    head
-  end
+  include NodeHelper
 
   def test_linked_list_cycle
     head = list_head
