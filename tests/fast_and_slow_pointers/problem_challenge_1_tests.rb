abe9788071777f3f3d8aa6186b8a6ec096b59245
see-why <iyadicy@gmail.com>
2023-08-29 08:31:12 +0100

    palindrome linkedlist solution

--- tests/fast_and_slow_pointers/problem_challenge_1_tests.rb
+++ tests/fast_and_slow_pointers/problem_challenge_1_tests.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+require_relative '../../fast_and_slow_pointers/problem_challenge_1'
+require_relative '../helpers/node_helper'
+require_relative '../../lib/node'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestFastAndSlowPointers < Test::Unit::TestCase
+  include NodeHelper
+
+  def test_palindrome_linkedlist
+    head = palindrome_head
+
+    assert_equal(true, palindrome_linkedlist?(head))
+
+    head.next.next.next.next.next = Node.new(2)
+    assert_equal(false, palindrome_linkedlist?(head))
+  end
+end
