389ed85b08a0b30937c2701999d1e1e88fe395a2
see-why <iyadicy@gmail.com>
2023-09-01 09:34:24 +0100

    refactor: account for even numbered nodes

--- fast_and_slow_pointers/middle_of_the_linkedlist.rb
+++ fast_and_slow_pointers/middle_of_the_linkedlist.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 def middle_of_the_linkedlist(head)
-  slow = head
-  fast = head.next
+  slow = fast = head
 
-  until fast.nil?
+  until fast.nil? || fast.next.nil?
     slow = slow.next
     fast = fast.next.nil? || fast.next.next.nil? ? nil : fast.next.next
   end
