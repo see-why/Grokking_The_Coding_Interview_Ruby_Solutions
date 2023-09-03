70ebc9d44755b53b49adbcb96eed61db71fb4212
see-why <iyadicy@gmail.com>
2023-09-03 22:43:13 +0100

    refactor: working solution

--- fast_and_slow_pointers/problem_challenge_3.rb
+++ fast_and_slow_pointers/problem_challenge_3.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
 def cycle_in_a_circular_array(arr)
-  fast = slow = index = steps = 0
-  max_index = arr.size
+  result = false
+  (0..arr.size - 1).each do |ind|
+    is_forward = arr[ind].positive?
+    slow = fast = ind
 
-  until
-    slow += 1
+    loop do
+      slow = find_next_index(arr, is_forward, slow)
+      fast = find_next_index(arr, is_forward, fast)
 
-    if arr[index].positive?
-      steps += arr[index]
-      steps % max_index if steps >= max_index
-    else
-      steps - arr[index]
-      while steps.negative?
-        steps += max_index
-      end
-      steps
+      fast = find_next_index(arr, is_forward, fast) if fast != -1
+      break if slow == fast || slow == -1 || fast == -1
     end
 
-    fast = steps
-
-    index += 1
+    result = true if slow != -1 && slow == fast
   end
+  result
+end
 
+def find_next_index(arr, is_forward, current_ind)
+  direction = arr[current_ind].positive?
+  return -1 if is_forward != direction
 
+  next_ind = (current_ind + arr[current_ind]) % arr.size
+  return -1 if next_ind == current_ind
+
+  next_ind
 end
