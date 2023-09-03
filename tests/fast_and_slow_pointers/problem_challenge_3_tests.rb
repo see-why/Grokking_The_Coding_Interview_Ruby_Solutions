b1ae16c0adbfb3d6c73a63ac0ef2e1cad6e73ed0
see-why <iyadicy@gmail.com>
2023-09-03 22:43:32 +0100

    test:

--- tests/fast_and_slow_pointers/problem_challenge_3_tests.rb
+++ tests/fast_and_slow_pointers/problem_challenge_3_tests.rb
@@ -1,2 +1,13 @@
 # frozen_string_literal: true
 
+require_relative '../../fast_and_slow_pointers/problem_challenge_3'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestFastAndSlowPointers < Test::Unit::TestCase
+  def test_cycle_in_a_circular_array
+    assert_equal(true, cycle_in_a_circular_array([1, 2, -1, 2, 2]))
+    assert_equal(true, cycle_in_a_circular_array([2, 2, -1, 2]))
+    assert_equal(false, cycle_in_a_circular_array([2, 1, -1, -2]))
+  end
+end
