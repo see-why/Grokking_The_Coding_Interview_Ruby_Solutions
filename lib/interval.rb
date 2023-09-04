de1df30a0d53b1d6b3a8c226e08d9f68805b64f5
see-why <iyadicy@gmail.com>
2023-09-04 23:18:57 +0100

    class: add interval

---
+++ lib/interval.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# represents an interval with an start and finish index
+class Interval
+  def initialize(start, finish)
+    @start = start
+    @end = finish
+  end
+end
