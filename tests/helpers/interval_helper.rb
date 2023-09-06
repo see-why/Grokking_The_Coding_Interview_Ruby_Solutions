2f4cac867d4de6adef940c42a6e634b64df09b74
see-why <iyadicy@gmail.com>
2023-09-06 23:48:07 +0100

    test: arguments for each case

---
+++ tests/helpers/interval_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/interval'
+
+def array_test_case_one
+  [Interval.new(1, 4), Interval.new(2, 5), Interval.new(7, 9)]
+end
+
+def array_test_case_two
+  [Interval.new(6, 7), Interval.new(2, 4), Interval.new(5, 9)]
+end
+
+def array_test_case_three
+  [Interval.new(1, 4), Interval.new(2, 6), Interval.new(3, 5)]
+end
