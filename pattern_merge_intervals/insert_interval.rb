8bc067bb60c6ed215be66ba4363cf69917e4a831
see-why <iyadicy@gmail.com>
2023-09-12 00:08:45 +0100

    insert interval solution

--- pattern_merge_intervals/insert_interval.rb
+++ pattern_merge_intervals/insert_interval.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
-def insert_interval; end
+def insert_interval(intervals, new_interval)
+  merged = []
+  i = 0
+
+  while (i < intervals.size) && (intervals[i].finish < new_interval.start)
+    merged << intervals[i].interval
+    i += 1
+  end
+
+  while (i < intervals.size) && (intervals[i].start <= new_interval.finish)
+    new_interval.start = [intervals[i].start, new_interval.start].min
+    new_interval.finish = [intervals[i].finish, new_interval.finish].max
+    i += 1
+  end
+
+  merged << new_interval.interval
+
+  intervals[i..].each { |item| merged << item.interval }
+  merged
+end
