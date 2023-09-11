029482af67b8020636c383b403e132e8cc1323a3
see-why <iyadicy@gmail.com>
2023-09-11 23:53:34 +0100

    test files

--- tests/pattern_merge_intervals/insert_interval_tests.rb
+++ tests/pattern_merge_intervals/insert_interval_tests.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+require_relative '../../pattern_merge_intervals/insert_interval'
+require_relative '../helpers/interval_helper'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestPatternMergeIntervals < Test::Unit::TestCase
+  def test_merge_interval
+    assert_equal([[1, 3], [4, 7], [8, 12]], insert_interval(array_test_case_one))
+    assert_equal([[1, 3], [4, 12]], insert_interval(array_test_case_two))
+    assert_equal([[1, 4], [5, 7]], insert_interval(array_test_case_three))
+  end
+end
