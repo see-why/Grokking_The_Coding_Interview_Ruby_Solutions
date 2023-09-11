9e3bd2a97e503669b451fb2a3ccf1f1c5d972f73
see-why <iyadicy@gmail.com>
2023-09-12 00:08:32 +0100

    insert interval test parameters

--- tests/helpers/interval_helper.rb
+++ tests/helpers/interval_helper.rb
@@ -13,3 +13,11 @@ end
 def array_test_case_three
   [Interval.new(1, 4), Interval.new(2, 6), Interval.new(3, 5)]
 end
+
+def interval_test_case_one_two
+  [Interval.new(1, 3), Interval.new(5, 7), Interval.new(8, 12)]
+end
+
+def interval_test_case_three
+  [Interval.new(2, 3), Interval.new(5, 7)]
+end
