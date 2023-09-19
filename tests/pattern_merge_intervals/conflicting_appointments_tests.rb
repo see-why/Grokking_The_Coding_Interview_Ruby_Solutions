8774f69deaf0a439c2a9bad43131bc8755ff1c2e
see-why <iyadicy@gmail.com>
2023-09-19 22:44:37 +0100

    tests: conflicting arrangements tests

--- tests/pattern_merge_intervals/conflicting_appointments_tests.rb
+++ tests/pattern_merge_intervals/conflicting_appointments_tests.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+require_relative '../../pattern_merge_intervals/conflicting_appointments'
+require_relative '../helpers/interval_helper'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestPatternMergeIntervals < Test::Unit::TestCase
+  def test_merge_interval
+    assert_equal(false, conflicting_appointments(conflicting_appointments_test_case_one))
+    assert_equal(true, conflicting_appointments(conflicting_appointments_test_case_two))
+    assert_equal(false, conflicting_appointments(conflicting_appointments_test_case_three))
+  end
+end
