9077009cfd78d47935fe8f96974a2e34ef317c15
see-why <iyadicy@gmail.com>
2023-09-19 22:35:01 +0100

    refactor: use explicit test case

--- tests/pattern_merge_intervals/merge_intervals_tests.rb
+++ tests/pattern_merge_intervals/merge_intervals_tests.rb
@@ -7,8 +7,8 @@ require 'test/unit'
 # test class for the Fast and Slow Pointers solutions
 class TestPatternMergeIntervals < Test::Unit::TestCase
   def test_merge_interval
-    assert_equal([[1, 5], [7, 9]], merge_intervals(array_test_case_one))
-    assert_equal([[2, 4], [5, 9]], merge_intervals(array_test_case_two))
-    assert_equal([[1, 6]], merge_intervals(array_test_case_three))
+    assert_equal([[1, 5], [7, 9]], merge_intervals(merge_intervals_test_case_one))
+    assert_equal([[2, 4], [5, 9]], merge_intervals(merge_intervals_test_case_two))
+    assert_equal([[1, 6]], merge_intervals(merge_intervals_test_case_three))
   end
 end
