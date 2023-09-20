b2c5de67ca88d41895dbbdc85035d3f09f0f5c69
see-why <iyadicy@gmail.com>
2023-09-20 23:46:21 +0100

    rename test methods

--- tests/pattern_merge_intervals/conflicting_appointments_tests.rb
+++ tests/pattern_merge_intervals/conflicting_appointments_tests.rb
@@ -6,7 +6,7 @@ require 'test/unit'
 
 # test class for the Fast and Slow Pointers solutions
 class TestPatternMergeIntervals < Test::Unit::TestCase
-  def test_merge_interval
+  def test_conflicting_appointments
     assert_equal(false, conflicting_appointments(conflicting_appointments_test_case_one))
     assert_equal(true, conflicting_appointments(conflicting_appointments_test_case_two))
     assert_equal(false, conflicting_appointments(conflicting_appointments_test_case_three))
