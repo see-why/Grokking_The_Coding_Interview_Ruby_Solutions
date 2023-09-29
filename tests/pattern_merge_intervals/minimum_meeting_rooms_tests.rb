d2dc4b67e40c16bab1aae896c08c0bead6f148f3
see-why <iyadicy@gmail.com>
2023-09-29 08:43:04 +0100

    update test result

--- tests/pattern_merge_intervals/minimum_meeting_rooms_tests.rb
+++ tests/pattern_merge_intervals/minimum_meeting_rooms_tests.rb
@@ -9,7 +9,7 @@ class TestPatternMergeIntervals < Test::Unit::TestCase
   def test_minimum_meeting_rooms
     assert_equal(2, minimum_meeting_rooms(minimum_meeting_rooms_test_case_one))
     assert_equal(1, minimum_meeting_rooms(minimum_meeting_rooms_test_case_two))
-    assert_equal(2, minimum_meeting_rooms(minimum_meeting_rooms_test_case_three))
+    assert_equal(3, minimum_meeting_rooms(minimum_meeting_rooms_test_case_three))
     assert_equal(2, minimum_meeting_rooms(minimum_meeting_rooms_test_case_four))
   end
 end
