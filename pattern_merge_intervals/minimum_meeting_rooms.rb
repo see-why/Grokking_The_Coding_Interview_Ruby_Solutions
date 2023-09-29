981f160061e2722e324d542eb2bbca1377c45e17
see-why <iyadicy@gmail.com>
2023-09-29 08:42:51 +0100

    implementation of minimum_meeting_rooms using a min heap

--- pattern_merge_intervals/minimum_meeting_rooms.rb
+++ pattern_merge_intervals/minimum_meeting_rooms.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
-def minimum_meeting_rooms; end
+require 'algorithms'
+
+def minimum_meeting_rooms(meetings)
+  meetings = meetings.sort_by(&:start)
+  rooms = 0
+  meeting_log = Containers::MinHeap.new
+
+  meetings.each do |interval|
+    meeting_log.pop while meeting_log.size.positive? && (interval.start >= meeting_log.min.finish)
+
+    meeting_log.push(interval)
+
+    rooms = [rooms, meeting_log.size].max
+  end
+
+  rooms
+end
