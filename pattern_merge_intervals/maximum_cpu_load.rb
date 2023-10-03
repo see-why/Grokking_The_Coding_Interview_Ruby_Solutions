1181d493595724361925e388335ce5954721eb25
see-why <iyadicy@gmail.com>
2023-10-03 23:49:53 +0100

    maximum_cpu_load at any time in a cpu given an array of jobs

--- pattern_merge_intervals/maximum_cpu_load.rb
+++ pattern_merge_intervals/maximum_cpu_load.rb
@@ -1,1 +1,16 @@
 # frozen_string_literal: true
+
+def maximum_cpu_load(jobs)
+  jobs = jobs.sort_by(&:start)
+  cpu_load = 0
+  job_tracker = []
+
+  jobs.each do |job|
+    job_tracker.pop while job_tracker.size.positive? && (job.start >= job_tracker.last.finish)
+
+    job_tracker << job
+    cpu_load = [cpu_load, job_tracker.map(&:cpu_load).inject(0, &:+)].max
+  end
+
+  cpu_load
+end
