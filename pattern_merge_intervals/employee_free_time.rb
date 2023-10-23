df341caf1cf862179dd5eeb69d0ff685c40761d2
see-why <iyadicy@gmail.com>
2023-10-23 02:59:39 +0100

    remove debugging logs

--- pattern_merge_intervals/employee_free_time.rb
+++ pattern_merge_intervals/employee_free_time.rb
@@ -25,23 +25,22 @@ def employee_free_time(employee_intervals)
   merged
 end
 
+# if each individual list is sorted!.
 def employee_free_time_with_heap(employee_intervals)
   result = []
-  interval_tracker = Containers::MinHeap.new
+  interval_tracker = Containers::Heap.new { |x, y| x.interval.start < y.interval.start }
 
   return result if employee_intervals.empty?
 
   employee_intervals.each_with_index do |item, ind|
     interval_tracker.push(EmployeeInterval.new(item.first, ind, 0))
   end
-  pp "heap: #{interval_tracker.inspect}"
-  pp "size: #{interval_tracker.size}"
-  previous_interval = interval_tracker.min.interval
+
+  previous_interval = interval_tracker.next.interval
 
   while interval_tracker.size.positive?
     heap_top = interval_tracker.pop
-    pp "heap_top: #{heap_top.inspect}"
-    pp "previous_interval: #{previous_interval.inspect}"
+
     if previous_interval.finish < heap_top.interval.start
       result << [previous_interval.finish, heap_top.interval.start]
       previous_interval = heap_top.interval
@@ -50,10 +49,11 @@ def employee_free_time_with_heap(employee_intervals)
     end
 
     employee_schedule = employee_intervals[heap_top.employee_index]
-    if employee_schedule.size > (heap_top.interval_index + 1)
-      interval_tracker.push(EmployeeInterval.new(employee_schedule[heap_top.interval_index + 1], heap_top.employee_index,
-                                                 heap_top.interval_index + 1))
-    end
+    next unless employee_schedule.size > (heap_top.interval_index + 1)
+
+    interval_tracker.push(EmployeeInterval.new(employee_schedule[heap_top.interval_index + 1],
+                                               heap_top.employee_index,
+                                               heap_top.interval_index + 1))
   end
 
   result = result.flatten if result.size == 1
