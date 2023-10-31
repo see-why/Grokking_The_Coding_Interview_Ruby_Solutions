fe854d1009aba44ccb46c2aa194d4373ceb3691d
see-why <iyadicy@gmail.com>
2023-10-31 10:10:23 +0100

    Additional test case

--- tests/pattern_cyclic_sort/find_the_missing_number_tests.rb
+++ tests/pattern_cyclic_sort/find_the_missing_number_tests.rb
@@ -8,5 +8,6 @@ class TestCyclicSort < Test::Unit::TestCase
   def test_find_the_missing_number
     assert_equal(2, find_the_missing_number([4, 0, 3, 1]))
     assert_equal(7, find_the_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+    assert_equal(4, find_the_missing_number([8, 3, 5, 2, 6, 0, 1, 9, 7, 10]))
   end
 end
