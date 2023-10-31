b2687efbe62bf8574a822bfdd5659c1da04284e8
see-why <iyadicy@gmail.com>
2023-10-31 10:09:21 +0100

    find_the_missing_number test cases

---
+++ tests/pattern_cyclic_sort/find_the_missing_number_tests.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../pattern_cyclic_sort/find_the_missing_number'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestCyclicSort < Test::Unit::TestCase
+  def test_find_the_missing_number
+    assert_equal(2, find_the_missing_number([4, 0, 3, 1]))
+    assert_equal(7, find_the_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+  end
+end
