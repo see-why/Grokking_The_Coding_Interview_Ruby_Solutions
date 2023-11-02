b209ba6307fe23916371d9c928af2e4ca6483010
see-why <iyadicy@gmail.com>
2023-11-02 23:57:06 +0100

    use array results

--- tests/pattern_cyclic_sort/find_all_missing_numbers_tests.rb
+++ tests/pattern_cyclic_sort/find_all_missing_numbers_tests.rb
@@ -7,7 +7,7 @@ require 'test/unit'
 class TestCyclicSort < Test::Unit::TestCase
   def test_find_the_missing_number
     assert_equal([4, 6, 7], find_all_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]))
-    assert_equal(3, find_all_missing_numbers([2, 4, 1, 2]))
-    assert_equal(4, find_all_missing_numbers([2, 3, 2, 1]))
+    assert_equal([3], find_all_missing_numbers([2, 4, 1, 2]))
+    assert_equal([4], find_all_missing_numbers([2, 3, 2, 1]))
   end
 end
