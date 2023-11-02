e692a286450ab7f33528efc40cc7bcd0886a18c4
see-why <iyadicy@gmail.com>
2023-11-02 23:57:42 +0100

    find_all_missing_numbers O(n) time complexity O(1) space complexity

--- pattern_cyclic_sort/find_all_missing_numbers.rb
+++ pattern_cyclic_sort/find_all_missing_numbers.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
-def find_all_missing_numbers; end
+def find_all_missing_numbers(arr)
+  result = []
+  i = 0
+  while i < arr.size
+    j = arr[i] - 1
+    if arr[i] != arr[j]
+      arr[i], arr[j] = arr[j], arr[i]
+    else
+      i += 1
+    end
+  end
+
+  arr.each_with_index do |val, ind|
+    result << (ind + 1) if val != ind + 1
+  end
+
+  result
+end
