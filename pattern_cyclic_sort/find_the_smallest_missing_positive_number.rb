787823d301fa737819bc7f5635f760d103454942
see-why <iyadicy@gmail.com>
2023-11-07 23:53:32 +0100

    Simpler solution

--- pattern_cyclic_sort/find_the_smallest_missing_positive_number.rb
+++ pattern_cyclic_sort/find_the_smallest_missing_positive_number.rb
@@ -3,20 +3,17 @@
 def find_the_smallest_missing_positive_number(arr)
   i = 0
   while i < arr.size
-    if arr[i] >= 0 && arr[i] != i && arr[i] < arr.size
-      j = arr[i]
+    j = arr[i] - 1
+    if arr[i].positive? && arr[i] != arr[j] && arr[i] <= arr.size
       arr[i], arr[j] = arr[j], arr[i]
     else
       i += 1
     end
   end
 
-  max_value = 0
   arr.each_with_index do |val, ind|
-    return ind if val.positive? && val != ind && ind.positive?
-
-    max_value = val if val.positive?
+    return ind + 1 if val != ind + 1
   end
 
-  (max_value + 1)
+  (arr.size + 1)
 end
