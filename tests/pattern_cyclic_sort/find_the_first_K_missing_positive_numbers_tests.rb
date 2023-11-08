ddc22da08b5e0a136a1905d9291284a77ef545ee
see-why <iyadicy@gmail.com>
2023-11-08 23:59:37 +0100

    find_the_k_missing_positive_numbers test cases

---
+++ tests/pattern_cyclic_sort/find_the_first_K_missing_positive_numbers_tests.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../pattern_cyclic_sort/find_the_k_missing_positive_numbers'
+require 'test/unit'
+
+# test class for the Fast and Slow Pointers solutions
+class TestCyclicSort < Test::Unit::TestCase
+  def test_find_the_k_missing_positive_numbers
+    assert_equal([1, 2, 6], find_the_k_missing_positive_numbers([3, -1, 4, 5, 5], 3))
+    assert_equal([1, 5, 6], find_the_k_missing_positive_numbers([2, 3, 4], 3))
+  end
+end
