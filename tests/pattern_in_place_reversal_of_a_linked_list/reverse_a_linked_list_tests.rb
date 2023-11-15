5d02468ee83b1162a0212edd62243e0c7b25b5e0
see-why <iyadicy@gmail.com>
2023-11-15 23:36:29 +0100

    Fix missing references

--- tests/pattern_in_place_reversal_of_a_linked_list/reverse_a_linked_list_tests.rb
+++ tests/pattern_in_place_reversal_of_a_linked_list/reverse_a_linked_list_tests.rb
@@ -8,6 +8,7 @@ require 'test/unit'
 # test class for the Fast and Slow Pointers solutions
 class TestReversalOfAlinkedList < Test::Unit::TestCase
   def test_reverse_a_linked_list
-    assert_equal([12, 10, 8, 6, 4, 2], find_the_first_k_missing_positive_numbers(rearrange_head))
+    head = rearrange_head
+    assert_equal([12, 10, 8, 6, 4, 2], reverse_a_linked_list(head))
   end
 end
