30eb6abf6ff1608577350c1545f6585930f16611
see-why <iyadicy@gmail.com>
2023-11-15 23:59:41 +0100

    reverse_a_linked_list O(n) algorithm

--- pattern_in_place_reversal_of_a_linked_list/reverse_a_linked_list.rb
+++ pattern_in_place_reversal_of_a_linked_list/reverse_a_linked_list.rb
@@ -1,2 +1,14 @@
 # frozen_string_literal: true
 
+def reverse_a_linked_list(head)
+  current = head
+  previous = nil
+  until current.nil?
+    next_node = current.next
+    current.next = previous
+    previous = current
+    current = next_node
+  end
+
+  previous.print_list
+end
